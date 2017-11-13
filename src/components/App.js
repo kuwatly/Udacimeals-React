fedd2fee87e4cc320c1290b9b6a250b148fc19b4
Iyad Kuwatly <ikuwatly@gmail.com>
2017-11-12 20:10:05 -0500

    Add mapStateToProps and mapDispatchToProps

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { addRecipe, removeFromCalendar } from "../actions/index";
 
 class App extends Component {
   render() {
@@ -10,4 +12,21 @@ class App extends Component {
   }
 }
 
-export default App
+function mapStateToProps (calendar) {
+  const dayOrder = ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday'];
+  return {
+    calendar: dayOrder.map((day) => ({
+      day,
+      meals: Object.assign({}, calendar[day])
+    }))
+  }
+}
+
+function mapDispatchToProps (dispatch) {
+  return {
+    selectRecipe: (data) => dispatch(addRecipe(data)),
+    remove: (data) => dispatch(removeFromCalendar(data))
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(App)
